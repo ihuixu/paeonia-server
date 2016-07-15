c741f28f765f59b176ade5b3296d02a061a22d7b
xh1021 <xh1021@vip.qq.com>
2016-07-15 15:37:21 +0800

    add

--- base/render.js
+++ base/render.js
@@ -5,10 +5,13 @@ var file = require('./file')
 module.exports = function(tplname, data){
 	var config = this.config[this.config.hosts[this.host]]
 	var viewsBase = path.join(config.hostPath, config.path.views)
+	var viewPath = path.join(viewsBase, tplname)
 
-	var tpl = file.readFile(path.join(viewsBase, tplname))
+	var tpl = file.readFile(viewPath)
 
-	this.body = ejs.render(tpl, data)
+	this.body = ejs.render(tpl, {
+		filename : viewPath 
+	})
 }
 
 
