d349ae757a6bb6ea798cce9f3ac0315a89b448d2
xh1021 <xh1021@vip.qq.com>
2016-07-15 14:04:16 +0800

    add

--- example/server.js
+++ example/server.js
@@ -1,12 +1,6 @@
 var server = require('paeonia-server')
 var config = require('./config.json')
-var fs = require('fs')
 
 server.start(config, __dirname)
 
-fs.createWriteStream("cmd/pids", {
-	flags: "a",
-	encoding: "utf-8",
-	mode: 0666
-}).write(process.pid + "\n");
 
