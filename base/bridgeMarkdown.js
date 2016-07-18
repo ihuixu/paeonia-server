cacaff02a199c5ad4fdd36aae2df2885088c4a7d
许 慧 <xh1021@vip.qq.com>
2016-07-19 06:36:06 +0800

    16

--- base/bridgeMarkdown.js
+++ base/bridgeMarkdown.js
@@ -13,21 +13,30 @@ marked.setOptions({
 })
 
 
-module.exports = function(filepath){
+module.exports = function(filepath, tag){
 	filepath = path.join(this.appConfig.hostPath, filepath)
 
 	this.bridge = function(){
 		return new Promise(function(resolve, reject){
+
+			var data = {}
+			var res = {}
+
 			try{
-				var data = file.readFile(filepath)
-				var html = marked(data)
-				resolve({code:0, data:html})
+				var md = file.readFile(filepath)
+				var html = marked(md)
+
+				res = {code:0, data:html}
 
 			}catch(err){
-				resolve({code:1, err:err})
+
+				res = {code:1, err:err}
 
 			}
 
+			data[tag || filepath] = res
+
+			resolve(data)
 		})
 	}
 }
