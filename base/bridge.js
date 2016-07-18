26b440a9b9d9f1b15ad0877c93cc8823e8e19fad
xh1021 <xh1021@vip.qq.com>
2016-07-18 16:45:01 +0800

    56

--- base/bridge.js
+++ base/bridge.js
@@ -1,17 +1,11 @@
+var path = require('path')
 var remoteApi = require('./remoteApi')
+var file = require('./file')
 
 module.exports = {	
 	bridgeMuch : function(php){
 		var mSelf = this
-		php = php || {}
-
-		if(this.php){
-			for(var i in this.php){
-				php[i] = this.php[i]
-			}
-		}
-
-		this.php = php
+		defaultAction.call(this, php)
 		
 		this.bridge = function(){
 			return new Promise(function(resolve, reject){
@@ -26,6 +20,37 @@ module.exports = {
 			})
 		}
 	}
+	, bridgeReadFile : function(filepath){
+		filepath = path.join(this.appConfig.hostPath, filepath)
+
+		this.bridge = function(){
+			return new Promise(function(resolve, reject){
+				try{
+					var data = file.readFile(filepath)
+					resolve(data)
+
+				}catch(err){
+					reject(err)
+
+				}
+
+			})
+		}
+	}
+
+}
+
+
+function defaultAction(php){
+	php = php || {}
+
+	if(this.php){
+		for(var i in this.php){
+			php[i] = this.php[i]
+		}
+	}
+
+	this.php = php
 }
 
 
