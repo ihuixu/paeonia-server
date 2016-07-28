e31be3302cf6a54d30451034a8816d85bc46562c
xh1021 <xh1021@vip.qq.com>
2016-07-28 16:39:09 +0800

    30

--- base/bridgeMuch.js
+++ base/bridgeMuch.js
@@ -1,8 +1,33 @@
-var remoteApi = require('./remoteApi')
+var request = require('request')
+
+function requestSync(options) {
+	return function (done) {
+		request(options, done)
+	}
+}
+
+
+var remoteApi = function*(){
+	var fns = {}
+	var php = this.php
+
+	for(var tag in php){
+		var arr = php[tag].split('::')
+
+		fns[tag] = yield requestSync({
+			url: 'http://' + this.config.apis[arr[0]] + arr[1] 
+			, json: true
+			, method: 'GET'
+		})
+	}
+
+	var data = yield fns
+
+	return data 
+}
+
 
 module.exports = function(php){
-	var mSelf = this
-
 	if(this.php){
 		for(var i in this.php){
 			php[i] = this.php[i]
@@ -13,7 +38,7 @@ module.exports = function(php){
 
 	this.bridge = function*(){
 
-		var data = yield remoteApi.call(mSelf, php)
+		var data = yield remoteApi.call(this)
 		
 		return data
 
