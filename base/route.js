5df1de30748187013e46ea7a78d704e3b27ca9fb
xh1021 <xh1021@vip.qq.com>
2016-07-29 12:56:27 +0800

    route

--- base/route.js
+++ base/route.js
@@ -4,23 +4,26 @@ var fs = require('fs')
 module.exports = function(){	
 	var controllerBase = path.join(this.appConfig.hostPath, this.appConfig.path.controller)
 
+	function getPath(routePath){
+		return path.join(controllerBase, (routePath || 'index')+'.js')
+	}
+
 	var url = this.url.split('?')[0]
 
 	if(url == '/favicon.ico'){
 		return;
 	}
 
-	url == '/' && (url = this.appConfig.hostDefault)
+	url == '/' && (url += (this.appConfig.hostDefault || 'index'))
 
 	var arr = url.split('/')
 	var args = arr.pop()
 	var method = 'index'
-	var controllerPath = path.join(controllerBase, arr.join('/')+'.js')
+	var controllerPath = getPath(arr.join('/'))
 
 	if(!fs.existsSync(controllerPath)){
 		method = arr.pop()
-
-		controllerPath = path.join(controllerBase, arr.join('/') + '.js')
+		controllerPath = getPath(arr.join('/'))
 	}
 
 	var controller = require(controllerPath)
